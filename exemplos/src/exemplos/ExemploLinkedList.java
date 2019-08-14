0c8c9f40003c75d21dafa52757284534577e718a
ccunha <carol.eletrica@gmail.com>
2019-08-14 16:32:39 +0100

    Add files via upload

---
+++ exemplos/src/exemplos/ExemploLinkedList.java
@@ -0,0 +1,13 @@
+package exemplos;
+
+import java.util.LinkedList;
+
+public class ExemploLinkedList {
+	public static void main(String[] args) {
+		LinkedList<String> exemplo = new LinkedList<String>();
+		exemplo.add("Estou estudando tópicos da linguagem Java.");
+		exemplo.add("Para isso, estou desenvolvendo alguns exemplos.");
+		exemplo.add("Nesse exemplo temos o LinkedList, que é uma lista encadeada.");
+	}
+
+}
